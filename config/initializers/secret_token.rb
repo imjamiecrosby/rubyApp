20559011dc497572632a8d941ff17feccfd3d47c
Aki Anderson <akianderson@Aki-Andersons-MacBook-Pro-2.local>
2014-07-19 12:20:03 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Imjamie::Application.config.secret_key_base = 'd115f7d83b44848dc8ae5140eb2fb38241ad814b08755a353f85d5192b2555e9491d1059965e4d9987337db4ec459c738222a15a896c95508b6b4788caf7a0b6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Imjamie::Application.config.session_store :cookie_store, key: '_imjamie_session'
